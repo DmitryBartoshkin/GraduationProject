b8f32ba332434b4b77d4d3bef0531f0ea639daea
Dmitry Bartoshkin <bartoshkin@gmail.com>
2015-02-21 11:53:51 +0300

    add header, footer templates

---
+++ app/views/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title><?php $this->title ?></title>
+	<link rel="stylesheet" type="text/css" href="/css/style.css"/>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.css" rel="stylesheet">
+</head>
+<body>
+<div class="navbar navbar-default" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Сайт компании</a>
+			<ul class="nav navbar-nav navbar-right">
+				<?php
+				if ( $this->session->isLoggedIn() ) {
+					echo '<li class="active"><a href="#">' . $this->session->getName() . '</a></li>';
+					echo '<li><a href="' . Controller::url( 'auth', 'logout' ) . '">Выйти</a></li>';
+				} else {
+					echo '<li><a href="' . Controller::url( 'auth', 'login' ) . '">Войти</a></li>';
+				}
+				?></li>
+			</ul>
+		</div>
+	</div>
+	<div class="navbar-collapse collapse">
+		<ul class="nav navbar-nav">
+			<li>
+				<a href="<?php Controller::url( 'user' ) ?>">Персонал</a>
+			</li>
+			<li>
+				<a href="<?php Controller::url( 'client' ) ?>">Клиенты</a>
+			</li>
+			<li>
+				<a href="<?php Controller::url( 'material' ) ?>">Материалы</a>
+			</li>
+		</ul>
+	</div>
+</div>
+<div class="container">
